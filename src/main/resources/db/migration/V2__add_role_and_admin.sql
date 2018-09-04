c4fc75b75fb37fc875eec1643db407ef5fa8eb26
Pavlo Tula <tpm.kiev.ua@gmail.com>
2018-09-04 13:50:43 +0300

    add log level

--- src/main/resources/db/migration/V2__add_role_and_admin.sql
+++ src/main/resources/db/migration/V2__add_role_and_admin.sql
@@ -1,8 +1,8 @@
 INSERT INTO roles(id, name) VALUES(1, 'USER');
 INSERT INTO roles(id, name) VALUES(2, 'ADMIN');
 
-INSERT INTO public.users (id, email, name, password, username) VALUES (1, 'admin@i.ua', 'admin', '$2a$10$QyT3z3a2//BD/3wzeTORAOoA6xlYqY8JUU2QVjPl2YKdYffLAoLR6', 'admin');
+INSERT INTO public.users (id, email, name, password, username) VALUES (1, 'admin@i.ua', 'admin', '$2a$10$Tz.bnrCWHwcY/xj8wjbPyOtIMxz0fgYZa1Q5g4lkUXqC2bKRauUSq', 'admin');
 INSERT INTO public.user_roles (user_id, role_id) VALUES (1, 2);
 
-INSERT INTO public.users (id, email, name, password, username) VALUES (2, 'user@i.ua', 'user', '$2a$10$QyT3z3a2//BD/3wzeTORAOoA6xlYqY8JUU2QVjPl2YKdYffLAoLR6', 'user');
+INSERT INTO public.users (id, email, name, password, username) VALUES (2, 'user@i.ua', 'user', '$2a$10$Tz.bnrCWHwcY/xj8wjbPyOtIMxz0fgYZa1Q5g4lkUXqC2bKRauUSq', 'user');
 INSERT INTO public.user_roles (user_id, role_id) VALUES (2, 1);
